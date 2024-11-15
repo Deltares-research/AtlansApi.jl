0c2014874671e09fd670de4c42348d09a36f60a3
smknaake <bas.knaake@deltares.nl>
2024-11-15 15:40:04 +0100

    Add test_rasterize

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Revise
 using AtlansApi
 using Atlans
 using JSON3
+using Rasters
 using Shapefile
 using Test
 
@@ -9,3 +10,4 @@ include("fixtures.jl")
 
 include("test_base.jl")
 include("test_build.jl")
+include("test_rasterize.jl")
