753dafe06bac7ee8ca2a5302e9d71c9d7253f7f2
smknaake <bas.knaake@deltares.nl>
2024-11-25 11:40:01 +0100

    Test shift_up, add_antropogenic, prepare_voxelstack

--- test/test_build.jl
+++ test/test_build.jl
@@ -70,6 +70,7 @@
     end
 
     @testset "shift_down" begin
+        # Test columns with negative depth values
         modelbase = -2.5
         
         surface = -0.05
@@ -90,24 +91,120 @@
         @test all(strat .== [2, 2, 2, 1])
         @test all(litho .== [3, 2, 1, 3])
 
+        # Test columns with positive depth values
         modelbase = 0
 
         surface = 2.3
         _, strat, litho = voxelstack_z_positive()
         Δz = fill(0.5, length(strat))
-
-        Δz, strat, litho = AtlansApi.shift_down(Δz, strat, litho, surface, modelbase)
+        Δz, _, _ = AtlansApi.shift_down(Δz, strat, litho, surface, modelbase)
         @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.3])
-        @test all(strat .== [2, 2, 2, 1, 1])
-        @test all(litho .== [3, 2, 1, 3, 2])
         
         surface = 2.05
         _, strat, litho = voxelstack_z_positive()
         Δz = fill(0.5, length(strat))
-
-        Δz, strat, litho = AtlansApi.shift_down(Δz, strat, litho, surface, modelbase)
+        Δz, _, _ = AtlansApi.shift_down(Δz, strat, litho, surface, modelbase)
         @test all(Δz .≈ [0.5, 0.5, 0.5, 0.55])
-        @test all(strat .== [2, 2, 2, 1])
-        @test all(litho .== [3, 2, 1, 3])
+    end
+
+    @testset "shift_up" begin
+        # Test columns with negative depth values
+        modelbase = -2.5
+        
+        surface = 0.3
+        _, strat, litho = voxelstack_z_negative()
+        Δz = fill(0.5, length(strat))
+
+        Δz, strat, litho = AtlansApi.shift_up(Δz, strat, litho, surface, modelbase)
+        @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.5, 0.3])
+        @test all(strat .== [2, 2, 2, 1, 1, 1])
+        @test all(litho .== [3, 2, 1, 3, 2, 2])
+        
+        surface = 0.05
+        _, strat, litho = voxelstack_z_negative()
+        Δz = fill(0.5, length(strat))
+
+        Δz, strat, litho = AtlansApi.shift_up(Δz, strat, litho, surface, modelbase)
+        @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.55])
+        @test all(strat .== [2, 2, 2, 1, 1])
+        @test all(litho .== [3, 2, 1, 3, 2])
+        
+        # Test columns with positive depth values
+        modelbase = 0
+
+        surface = 2.7
+        _, strat, litho = voxelstack_z_positive()
+        Δz = fill(0.5, length(strat))
+        Δz, _, _ = AtlansApi.shift_up(Δz, strat, litho, surface, modelbase)
+        @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.5, 0.2])
+        
+        surface = 2.54
+        _, strat, litho = voxelstack_z_positive()
+        Δz = fill(0.5, length(strat))
+        Δz, _, _ = AtlansApi.shift_up(Δz, strat, litho, surface, modelbase)
+        @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.54])
+    end
+
+    @testset "add_antropogenic" begin
+        _, strat, litho = voxelstack_z_negative()
+        Δz = fill(0.5, length(strat))
+
+        Δz, strat, litho = AtlansApi.add_antropogenic(Δz, strat, litho, 2.3)
+        @test all(Δz .≈ [0.5, 0.5, 0.5, 0.5, 0.5, 2.3])
+        @test all(strat .== [2, 2, 2, 1, 1, 1])
+        @test all(litho .== [3, 2, 1, 3, 2, 0])
+    end
+
+    @testset "prepare_voxelstack" begin
+        z, strat, litho = voxelstack_z_positive()
+
+        # Test voxelstack surface is shifted down
+        surface = 2.3
+        domain = AtlansApi.prepare_voxelstack(z, surface, strat, litho)
+        @test isa(domain, Atlans.VerticalDomain)
+        @test all(
+            domain.z .≈ [
+                0.125, 0.375, 0.625, 0.875, 1.125, 1.375, 1.625, 1.875, 2.075, 2.225
+            ]
+        )
+        @test all(
+            domain.Δz .≈ [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.15, 0.15]
+        )
+        @test all(domain.geology .== [2, 2, 2, 2, 2, 2, 1, 1, 1, 1])
+        @test all(domain.lithology .== [3, 3, 2, 2, 1, 1, 3, 3, 2, 2])
+        @test all(domain.index .== [1, 1, 2, 2, 3, 3, 4, 4, 5, 5])
+        @test domain.n == 10
+
+        # Test voxelstack surface is shifted up
+        surface = 2.7
+        domain = AtlansApi.prepare_voxelstack(z, surface, strat, litho)
+        @test all(
+            domain.z .≈ [
+                0.125, 0.375, 0.625, 0.875, 1.125, 1.375, 1.625, 1.875, 2.125, 2.375, 2.6
+                ]
+                )
+                @test all(
+                    domain.Δz .≈ [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.2]
+                    )
+                    @test all(domain.geology .== [2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1])
+                    @test all(domain.lithology .== [3, 3, 2, 2, 1, 1, 3, 3, 2, 2, 2])
+                    @test all(domain.index .== [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6])
+                    @test domain.n == 11
+                    
+        # Test antropogenic is added to voxelstack            
+        surface = 5
+        domain = AtlansApi.prepare_voxelstack(z, surface, strat, litho)
+        @test all(domain.z .≈ 0.125:0.25:4.875)
+        @test all(domain.Δz .≈ 0.25)
+        @test all(
+            domain.geology .== [2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        )
+        @test all(
+            domain.lithology .== [3, 3, 2, 2, 1, 1, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        )
+        @test all(
+            domain.index .== [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+        )
+        @test domain.n == 20
     end
 end
