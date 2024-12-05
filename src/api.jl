99a6b10e40aef0da34df8cf2ed6b79688acc67dc
smknaake <bas.knaake@deltares.nl>
2024-12-05 15:41:25 +0100

    Make API call return unique hash and tif paths, write output to tifs

--- src/api.jl
+++ src/api.jl
@@ -1,3 +1,21 @@
+"""
+	run_model(req::HTTP.Request)
+
+Process an HTTP request to run an Atlantis model simulation for surcharge calculations
+based on the provided input data.
+
+# Request JSON Structure
+- `gw`: A numerical value representing the groundwater level.
+- `geojson`: A GeoJSON object representing the geographical features.
+
+# Response JSON Structure
+- `id`: A unique identifier for the generated directory.
+- `initial`: The file path to the initial TIFF file.
+- `remaining`: The file path to the remaining TIFF file.
+
+# Errors
+- Returns a 400 HTTP response with an error message if the input is invalid.
+"""
 function run_model(req::HTTP.Request)
 	try
 		data = JSON3.read(String(req.body))
@@ -11,11 +29,15 @@ function run_model(req::HTTP.Request)
 			remaining_tif = "$OUTPUT_PATH/$folder_id/remaining.tif"
 
 			result = run_model(features, gw)
+			write(result, initial_tif, remaining_tif)
 
 			return HTTP.Response(200, JSON3.write(
-				Dict("id" => folder_id, "initial" => initial_tif, "remaining" => remaining_tif),
-			)
-			)
+				Dict(
+					"id" => folder_id,
+					"initial" => initial_tif,
+					"remaining" => remaining_tif,
+				),
+			))
 		end
 	catch e
 		return HTTP.Response(400, JSON3.write(Dict("error" => "Invalid input")))
@@ -23,6 +45,11 @@ function run_model(req::HTTP.Request)
 end
 
 
+"""
+	session(host::String, port::Int)
+
+Start an HTTP server session to listen for incoming requests to run Atlantis model simulations.
+"""
 function session(host::String, port::Int)
 	router = HTTP.Router() do req
 		println("HTTP: \"$(req.method) $(req.target)\"")
@@ -38,6 +65,12 @@ function session(host::String, port::Int)
 end
 
 
+"""
+	generate_unique_directory_id()
+
+Helper function to generate a unique directory id-hash for different API calls to store
+model output tifs.
+"""
 function generate_unique_directory_id()
 	# While id does exist generate a new id
 	id = string(UUIDs.uuid4())
